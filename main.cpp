dc58eb7a865cb2757b3b5f6f5f541da9f1edc4c6
hrp4cruser@hrp4cr.com <hrp4cruser@hrp4cr.com>
2023-04-05 13:02:40 +0900

    Improved test program to accept any angle

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string.h>
 #include "RS405CB.h"
 
 int main(int argc, char *argv[])
 {
 	int id = 0;
-	if(argc == 2)
-		id = atoi(argv[1]);
+	double ang = 0;
+	const char *dev = "/dev/ttyUSB0";
 
-	RS405CB servo("/dev/ttyUSB0", 115200);
+	for (int i = 1; i < argc; i++) {
+	  if (strcmp(argv[i], "-id") == 0) {
+	    id = atoi(argv[++i]);
+	  }
+	  else if (strcmp(argv[i], "-dev") == 0) {
+	    dev = argv[++i];
+	  }
+	  else if (strcmp(argv[i], "-ang") == 0) {
+	    ang = atof(argv[++i]);
+	  }
+	}
+	
+	RS405CB servo(dev, 115200);
 
 	const double voltage = servo.getVoltage(id);
 	std::cout << "voltage: " << voltage << std::endl;
 	const int temperature = servo.getTemperature(id);
 	std::cout << "temperature: " << temperature << std::endl;
-	std::cout << "angle: " << servo.getAngle(id) << std::endl;
+	const int torqueEnable = servo.getTorqueEnable(id);
+	std::cout << "torque enable (before): " << torqueEnable << std::endl;
+	const double angle = servo.getAngle(id);
+	std::cout << "angle (before): " << angle << std::endl;
 	servo.setTorque(id, true); // torque on
-	servo.setAngle(id, 0.0);
+	servo.setAngle(id, ang);
 
 	return 0;
 }
